5466e1724d3bc9bb9e765c1c15d1c799116b44bd
karnakar <karnakar@ymail.com>
2020-01-01 14:03:02 -0500

    eureka setup

--- src/main/java/kt/netflix/eureka/naming/server/KtNetflixEurekaNamingServerApplication.java
+++ src/main/java/kt/netflix/eureka/naming/server/KtNetflixEurekaNamingServerApplication.java
@@ -2,8 +2,11 @@ package kt.netflix.eureka.naming.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class KtNetflixEurekaNamingServerApplication {
 
 	public static void main(String[] args) {
