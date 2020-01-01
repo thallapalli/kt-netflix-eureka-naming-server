ddebca4537f6fb0705418e68d96c0eb0fe077658
karnakar <karnakar@ymail.com>
2020-01-01 13:55:06 -0500

    first commit

---
+++ src/main/java/kt/netflix/eureka/naming/server/KtNetflixEurekaNamingServerApplication.java
@@ -0,0 +1,13 @@
+package kt.netflix.eureka.naming.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtNetflixEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtNetflixEurekaNamingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kt/netflix/eureka/naming/server/KtNetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kt.netflix.eureka.naming.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtNetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
